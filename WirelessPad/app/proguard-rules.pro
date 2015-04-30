0ad6ce34176d762af6a5633d9b55ea6a54c22269
yksz <user4git@gmail.com>
2015-05-01 00:21:14 +0900

    Upgrade to the Android Studio 1.1.0

--- WirelessPad/app/proguard-rules.pro
+++ WirelessPad/app/proguard-rules.pro
@@ -1,6 +1,6 @@
 # Add project specific ProGuard rules here.
 # By default, the flags in this file are appended to flags specified
-# in C:\android-studio\sdk/tools/proguard/proguard-android.txt
+# in $ANDROID_SDK_ROOT/tools/proguard/proguard-android.txt
 # You can edit the include path and order by changing the proguardFiles
 # directive in build.gradle.
 #
--- WirelessPad/app/src/main/groovy/jp/android/wirelesspad/MainActivity.groovy
+++ WirelessPad/app/src/main/groovy/jp/android/wirelesspad/MainActivity.groovy
@@ -18,8 +18,8 @@ class MainActivity extends ActionBarActivity {
 
     @Override
     boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu)
+        // Inflate the menu this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu)
         return true
     }
 
@@ -29,6 +29,7 @@ class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId()
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true
         }

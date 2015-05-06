62140534495adeac00010eb91ae1e75fa035f1eb
yksz <user4git@gmail.com>
2015-05-07 01:40:47 +0900

    Create the activity for a pad

---
+++ WirelessPad/app/src/main/groovy/jp/android/wirelesspad/ui/FullscreenActivity.groovy
@@ -0,0 +1,70 @@
+package jp.android.wirelesspad.ui
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+
+import groovy.transform.CompileStatic
+
+import jp.android.wirelesspad.R
+import jp.android.wirelesspad.ui.util.SystemUiHider
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ *
+ * @see SystemUiHider
+ */
+@CompileStatic
+public class FullscreenActivity extends Activity {
+    /**
+     * The flags to pass to {@link SystemUiHider#getInstance}.
+     */
+    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION
+
+    /**
+     * The instance of the {@link SystemUiHider} for this activity.
+     */
+    private SystemUiHider mSystemUiHider
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fullscreen)
+
+        View controlsView = findViewById(R.id.fullscreen_content_controls)
+        View contentView = findViewById(R.id.fullscreen_content)
+
+        // Set up an instance of SystemUiHider to control the system UI for
+        // this activity.
+        mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS)
+        mSystemUiHider.setup()
+        mSystemUiHider.setOnVisibilityChangeListener { visible ->
+            controlsView.setVisibility(visible ? View.VISIBLE : View.GONE)
+        }
+
+        // Set up the user interaction to manually show or hide the system UI.
+        contentView.setOnLongClickListener { view ->
+            mSystemUiHider.toggle()
+            return false
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume()
+        if (mSystemUiHider.isVisible()) {
+            mSystemUiHider.hide()
+        }
+    }
+
+    public void onClickSettingsButton(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class)
+        startActivity(intent)
+    }
+
+    public void onClickCloseButton(View view) {
+        mSystemUiHider.hide()
+    }
+}
--- WirelessPad/app/src/main/groovy/jp/android/wirelesspad/ui/SettingsActivity.groovy
+++ WirelessPad/app/src/main/groovy/jp/android/wirelesspad/ui/SettingsActivity.groovy
@@ -1,38 +1,43 @@
-package jp.android.wirelesspad
+package jp.android.wirelesspad.ui
 
 import android.support.v7.app.ActionBarActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+
 import groovy.transform.CompileStatic
 
+import jp.android.wirelesspad.R
 
 @CompileStatic
-class MainActivity extends ActionBarActivity {
+public class SettingsActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_settings)
     }
 
+
     @Override
-    boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu)
+        getMenuInflater().inflate(R.menu.menu_settings, menu)
         return true
     }
 
     @Override
-    boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId()
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true
         }
+
         return super.onOptionsItemSelected(item)
     }
 }

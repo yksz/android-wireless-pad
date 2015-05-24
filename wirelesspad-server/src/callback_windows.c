e71dd08c337beea99fa70c300d5b090267a56cb4
yksz <user4git@gmail.com>
2015-05-24 22:34:34 +0900

    Add new mouse events in wirelesspad-server

--- wirelesspad-server/src/callback_windows.c
+++ wirelesspad-server/src/callback_windows.c
@@ -9,10 +9,40 @@
 
 static void moveCursor(int x, int y)
 {
-    printf("move %d %d\n", x, y);
+    lwsl_debug("moveCursor: x=%d, y=%d\n", x, y);
     POINT point = {0};
     GetCursorPos(&point);
     SetCursorPos(point.x + x, point.y + y);
+}
+
+static void scroll(int amount)
+{
+    lwsl_debug("scroll: amount=%d\n", amount);
+    mouse_event(MOUSEEVENTF_WHEEL, 0, 0, amount, 0);
+}
+
+static void leftClick()
+{
+    lwsl_debug("leftClick\n");
+    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+    Sleep(10);
+    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+}
+
+static void rightClick()
+{
+    lwsl_debug("rightClick\n");
+    mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+    Sleep(10);
+    mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+}
+
+static void doubleClick()
+{
+    lwsl_debug("doubleClick\n");
+    leftClick();
+    Sleep(GetDoubleClickTime() * 0.5);
+    leftClick();
 }
 
 static void parseReceiveMessage(char* message, size_t len)
@@ -27,11 +57,17 @@ static void parseReceiveMessage(char* message, size_t len)
         int x = (tok != NULL) ? atoi(tok) : 0;
         tok = strtok(NULL, delimiter);
         int y = (tok != NULL) ? atoi(tok) : 0;
-        moveCursor(x, y);
-    } else if (strncmp(tok, "leftclick", len) == 0) {
-        printf("leftclick\n");
-    } else if (strncmp(tok, "rightclick", len) == 0) {
-        printf("rightclick\n");
+        moveCursor(-x, -y);
+    } else if (strncmp(tok, "scroll", len) == 0) {
+        tok = strtok(NULL, delimiter);
+        int amount = (tok != NULL) ? atoi(tok) : 0;
+        scroll(-amount);
+    } else if (strncmp(tok, "leftClick", len) == 0) {
+        leftClick();
+    } else if (strncmp(tok, "rightClick", len) == 0) {
+        rightClick();
+    } else if (strncmp(tok, "doubleClick", len) == 0) {
+        doubleClick();
     } else {
         return;
     }
